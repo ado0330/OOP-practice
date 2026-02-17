495edfa20250a684b643f074ecd624fcd5cb6eeb
Z X KHAW <jasonkhaw0330@gmail.com>
2026-02-17 22:05:07 +0800

    Update RPG-game.cpp
    -Make menu
    -Add two new function , Gold and Weapon
    - Add Shopping System, player can buy weapon to help in the battle

--- RPG-game.cpp
+++ RPG-game.cpp
@@ -11,13 +11,18 @@ protected:
     int HealthPoints;
     int ATK;
     int ManaPoints;
+    int Gold = 1000000;
+    string Weapon;
 public:
-    Entity(string name, int hp, int atk, int mp) {
+    Entity(string name, int hp, int atk, int mp, string weapon) {
         Name = name;
         HealthPoints = hp;
         ATK = atk;
         ManaPoints = mp;
+        Weapon = weapon;
     }
+
+    virtual ~Entity(){};
 
     string getName() {
         return Name;
@@ -27,12 +32,28 @@ public:
         return ATK;
     }
 
+    int getGold() {
+        return Gold;
+    }
+
+    void spendGold(int amount) {
+        Gold -= amount;
+    }
+
+    void setWeapon(string weapon) {
+        Weapon = weapon;
+    }
+
     void AddMP(int mp) {
         ManaPoints += mp;
     }
 
     void TakeDamage(int damage) {
         HealthPoints -= damage;
+    }
+
+    void AddGold(int amount) {
+        Gold += amount;
     }
 
 
@@ -56,8 +77,26 @@ public:
     }
 
     virtual void UseSkill(Entity & target) {}
+    virtual string getClassName() = 0;
+    virtual void ShowStatus() {
+        cout << "---------Status---------" << endl;
+        cout << "Name: " << Name << endl;
+        cout << "Class: " << getClassName() << endl; 
+        cout << "HP: " << HealthPoints << endl;
+        cout << "ATK: " << ATK << endl;
+        cout << "MP: " << ManaPoints << endl;
+        cout << "Weapon: " << Weapon << endl;
+        cout << "Gold: " << getGold() << endl;
+        cout << "------------------------" << endl;
+    
+    }
 
-    virtual void ShowStatus() {}
+    void WeaponBuff(int hp, int atk, int mp) {
+        HealthPoints += hp;
+        ATK += atk;
+        ManaPoints += mp;
+
+    }
 
     
 
@@ -66,28 +105,20 @@ public:
 
 class Warrior:public Entity {
 public:
-    Warrior(string name):Entity(name, 100, 50, 0) {}
+    Warrior(string name):Entity(name, 100, 25, 0, "Longsword") {}
+    string getClassName() {return "Warrior";}
     void UseSkill(Entity &target) override{
             int totalDamage = ATK * 2 + rand() % 10;
             cout << "Tactical Attack!" << endl;
             target.TakeDamage(totalDamage);
             cout << Name << " deals " << totalDamage << " damage to " << target.getName() << "." << endl;
         }
-    void ShowStatus() override{
-        cout << "---------Status---------" << endl;
-        cout << "Name:" << Name << endl;
-        cout << "Class: Warrior" << endl;
-        cout << "HP: " << HealthPoints << endl;
-        cout << "ATK: " << ATK << endl;
-        cout << "MP: " << ManaPoints << endl;
-        cout << "------------------------" << endl;
-        
-    }
 };
 
 class Mage: public Entity {
 public:
-    Mage(string name):Entity(name, 100, 30, 50) {}
+    Mage(string name):Entity(name, 80, 30, 50, "Wand") {}
+    string getClassName() {return "Mage";}
     const int FIREBALL_COST = 25;
     void UseSkill(Entity &target) override{
         int totalDamage = ATK * 3;
@@ -101,24 +132,83 @@ public:
                 BasicAttack(target);
             }
         }
-    void ShowStatus() override{
-        cout << "---------Status---------" << endl;
-        cout << "Name:" << Name << endl;
-        cout << "Class: Mage" << endl;
-        cout << "HP: " << HealthPoints << endl;
-        cout << "ATK: " << ATK << endl;
-        cout << "MP: " << ManaPoints << endl;
-        cout << "------------------------" << endl;
-    }
 };
 
 class Slime:public Entity {
 public:
-    Slime():Entity("Slime", 300, 10, 0) {}
+    Slime():Entity("Slime", 300, 15, 0, "") {}
     void UseSkill(Entity &target) override {
         BasicAttack(target);
     }
+    string getClassName() {return "Monster";}
 };
+
+void goToCave(Entity *player) {
+    //Battle Logic
+    Slime slime;
+    int amount = 100;
+    cout << "\n--- You entered a dark cave and found a Slime! ---" << endl;
+    while (player->IsAlive() && slime.IsAlive()) {
+        player->ShowStatus();
+        player->UseSkill(slime);
+        
+        if (slime.IsAlive()) {
+            slime.BasicAttack(*player); 
+        }
+        player->AddMP(5); 
+    }   
+    if (player->IsAlive()) {
+        player->AddGold(amount);
+        cout << "You WON!!" << endl;
+        cout << "You gain " << amount << " gold!" << endl;
+        cout << "Now you have " << player->getGold() << " gold!" << endl;
+    } else {
+        cout << "You were defeated..." << endl;
+    }
+}
+
+void goToShop(Entity *player) {
+    int choice = 0;
+    const int AMOUNT_WARRIOR_WEAPON = 300;
+    const int AMOUNT_MAGE_WEAPON = 300;
+    const string WEAPON_WARRIOR = "Katana";
+    const string WEAPON_MAGE = "Wand of Sparking";
+
+    do {
+    cout << "---WELCOME TO WEAPON STORE---" << endl;
+    cout << "1. "<< WEAPON_WARRIOR << "(Warrior)            --- " << AMOUNT_WARRIOR_WEAPON << " Gold" << endl;
+    cout << "2. "<< WEAPON_MAGE << " (Mage)     --- " << AMOUNT_MAGE_WEAPON << " Gold" << endl;
+    cout << "3. Exit Shop" << endl;
+    cout << "Enter your choice: " << endl;
+    cin  >> choice;
+
+    if (choice == 3) {
+        break;
+    }
+    
+    int gold = player->getGold();
+    string className = player->getClassName();
+
+    if (gold >= 300 && className == "Warrior" && choice == 1) {
+        cout << "Purchase successful!" << endl;
+        player->spendGold(AMOUNT_WARRIOR_WEAPON);
+        player->setWeapon(WEAPON_WARRIOR);
+        player->WeaponBuff(10, 10, 0);
+        
+
+    } else if (gold >= 300 && className == "Mage" && choice == 2) {
+        cout << "Purchase successful!" << endl;
+        player->spendGold(AMOUNT_MAGE_WEAPON);
+        player->setWeapon(WEAPON_MAGE);
+        player->WeaponBuff(5, 15, 15);
+    } else {
+        cout << "Purchase unsuccessful.\n Please check you buy the weapon match with your class!" << endl;
+    }
+
+    } while (true);
+
+
+}
 
 
 
@@ -133,6 +223,7 @@ int main() {
     cout << "Enter your name: " << endl;
     getline(cin, name);
 
+    //Initilize the player class
     do {
         cout << "----------SELECT YOUR CLASS----------" << endl;
         cout << "1. Warrior" << endl;
@@ -154,28 +245,36 @@ int main() {
 
     }
 
-    Slime slime;
 
+    //Menu
+    bool gameRunning = true;
+    while (gameRunning && player->IsAlive()) {
+        int action;
+        cout << "\nWhat would you like to do?" << endl;
+        cout << "1. Go to Cave (Battle)" << endl;
+        cout << "2. Go to Shop (Upgrade)" << endl;
+        cout << "3. Check Status" << endl;
+        cout << "4. Quit Game" << endl;
+        cin >> action;
 
-
-    cout << "---------------Battle Start---------------" << endl;
-    while (player->IsAlive() && slime.IsAlive()) {
-        player->ShowStatus();
-        player->UseSkill(slime);
-        
-        if (slime.IsAlive()) {
-            slime.BasicAttack(*player); 
+        switch(action) {
+            case 1:
+                goToCave(player);
+                break;
+            case 2:
+                goToShop(player);
+                break;
+            case 3:
+                player->ShowStatus();
+                break;
+            case 4:
+                gameRunning = false;
+                break;
         }
-        player->AddMP(5);
     }
 
-    if (player->IsAlive()) {
-        cout << "You WON!!" << endl;
-    } else {
-        cout << "GAME OVER" << endl;
-    }
-
-    delete [] player;
+    delete player;
+    player = nullptr;
 
     return 0;
 }
